44029d237245c58de36ceb1845d19961b3dc2b82
Will Anderson <will@itsananderson.com>
2015-07-10 08:25:03 -0700

    Add Gulpfile and some very basic tests

---
+++ test/basic.js
@@ -0,0 +1,15 @@
+var createHateoas = require("../");
+var assert = require("assert");
+
+describe("hateoas", function() {
+    it("can be called", function() {
+        var hateoas = createHateoas("http://localhost/");
+        assert(hateoas);
+    });
+
+    it("throws for missing baseUrl", function() {
+        assert.throws(function() {
+            var hateoas = createHateoas();
+        }, "Should have thrown for missing baseUrl");
+    });
+});
